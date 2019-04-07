700e0c8c86b279930e3363a912679729283a0119
MikeDoesStuff <mikey.hughes0152@googlemail.com>
2019-04-07 12:34:05 +0100

    main menu proj
    main menu project added

---
+++ New Unity Project/Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void PlayGame () {
+        Debug.Log("Starting Game...");
+        SceneManager.LoadScene(1);
+	}
+	
+	// Update is called once per frame
+	public void QuitGame () {
+        Debug.Log("Quitting...");
+        Application.Quit();
+	}
+
+}
---
+++ New Unity Project/Assets/SliderScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SliderScript : MonoBehaviour {
+
+    // Use this for initialization
+    public AudioMixer mixer;
+
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+    }
+}
+
